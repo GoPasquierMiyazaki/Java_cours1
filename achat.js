5b0946b507a6ccca2173225731bf8c3bc5f5b593
GoPasquierMiyazaki <113765036+GoPasquierMiyazaki@users.noreply.github.com>
2022-12-18 21:45:03 +0100

    Delete achat.js

--- achat.js
+++
@@ -1,7 +0,0 @@
-const body = document.querySelector("body");
-
-const buttonDL = document.querySelector("#btn1");
-
-buttonDL.addEventListener("click", function() {
-  body.classList.toggle("nightMode");
-})
