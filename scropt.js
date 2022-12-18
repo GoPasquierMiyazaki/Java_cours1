0ae3056fb96a74610c0d66b1087af6c1c9590b5e
GoPasquierMiyazaki <113765036+GoPasquierMiyazaki@users.noreply.github.com>
2022-12-18 19:38:49 +0100

    Delete scropt.js

--- scropt.js
+++
@@ -1,7 +0,0 @@
-const body = document.querySelector(".body");
-
-const button = document.querySelector("#btn1");
-
-button.addEventListener("click", function() {
-  body.classList.toggle("nightMode");
-})
